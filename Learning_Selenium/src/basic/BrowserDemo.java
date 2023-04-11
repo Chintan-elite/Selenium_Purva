0cc9f7c4fd2c5f5309ea99562dadccbd23e6a1ca
chintan <chintan.tops@gmail.com>
2023-04-11 08:32:19 +0530

    selenium

---
+++ Learning_Selenium/src/basic/BrowserDemo.java
@@ -0,0 +1,19 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\selenuum data\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+	}
+}
